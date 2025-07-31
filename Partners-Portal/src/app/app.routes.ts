1887a44628c538d1d69e06001878e9c9828069df
calebkg <calebkariuki05@gmail.com>
2025-07-31 17:28:06 +0300

    Create Payment Surrender Components with Similar Design

--- Partners-Portal/src/app/app.routes.ts
+++ Partners-Portal/src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { PaymentRequestComponent } from './components/payment-request/payment-request.component';
 import { NewPaymentRequestComponent } from './components/payment-request/new-payment-request/new-payment-request.component';
+import { PaymentSurrenderComponent } from './components/payment-surrender/payment-surrender.component';
+import { NewPaymentSurrenderComponent } from './components/payment-surrender/new-payment-surrender/new-payment-surrender.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
@@ -11,6 +13,8 @@ export const routes: Routes = [
   { path: 'payment-request', component: PaymentRequestComponent },
   { path: 'new-payment-request', component: NewPaymentRequestComponent },
   { path: 'new-payment-request/:id', component: NewPaymentRequestComponent },
-  { path: 'payment-surrender', component: PaymentRequestComponent }, // Using payment request as placeholder
+  { path: 'payment-surrender', component: PaymentSurrenderComponent },
+  { path: 'new-payment-surrender', component: NewPaymentSurrenderComponent },
+  { path: 'new-payment-surrender/:id', component: NewPaymentSurrenderComponent },
   { path: 'reimbursements', component: PaymentRequestComponent }, // Using payment request as placeholder
 ];
